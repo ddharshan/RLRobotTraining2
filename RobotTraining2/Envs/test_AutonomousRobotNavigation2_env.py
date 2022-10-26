b0cff6cdb12ee43ba1d94c9db828b047f27c52c9
Dharshan <108181384+ddharshan@users.noreply.github.com>
2022-10-26 10:29:38 +0530

    Create test_AutonomousRobotNavigation2_env.py

---
+++ RobotTraining2/Envs/test_AutonomousRobotNavigation2_env.py
@@ -0,0 +1,31 @@
+import pytest
+import numpy as np
+from easydict import EasyDict
+
+from RobotTraining2.Envs import AutoRobot2CEnv #change-----------------------------------------------------------
+
+
+@pytest.mark.envtest
+class TestAutoRobot2CEnv:  #change-----------------------------------------------------
+
+    def test_naive(self):
+        env = AutoRobot2CEnv(EasyDict({'env_id': 'Moving-v0', 'act_scale': False})) #change----------------------------------------------
+        env.enable_save_replay('./video')
+        env.seed(314, dynamic_seed=False)
+        assert env._seed == 314
+        obs = env.reset()
+        assert obs.shape == (28, )  #HERE-----------------------------------------------------------------------
+        for i in range(300):  #change-----------------------------------------------------
+            random_action = env.random_action()
+            print('random_action', random_action)
+            timestep = env.step(random_action)
+            assert isinstance(timestep.obs, np.ndarray)
+            assert isinstance(timestep.done, bool)
+            assert timestep.obs.shape == (28, )    #HERE-------------------------------------------------------------------
+            assert timestep.reward.shape == (1, )
+            assert timestep.info['action_args_mask'].shape == (3, 2)
+            if timestep.done:
+                print('reset env')
+                env.reset()
+        print(env.observation_space, env.action_space, env.reward_space)
+        env.close()
