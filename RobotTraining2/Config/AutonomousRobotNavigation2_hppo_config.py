0c74ac4f8473ce703770afaec3f6d15bad003e64
Dharshan <108181384+ddharshan@users.noreply.github.com>
2022-11-14 09:44:13 +0530

    Update AutonomousRobotNavigation2_hppo_config.py

--- RobotTraining2/Config/AutonomousRobotNavigation2_hppo_config.py
+++ RobotTraining2/Config/AutonomousRobotNavigation2_hppo_config.py
@@ -11,7 +11,7 @@ AutonomousRobotNavigation2_hppo_config = dict(
         act_scale=True,
         env_id='Moving-v0',  # ['Sliding-v0', 'Moving-v0']
         n_evaluator_episode=5,
-        stop_value=1.8,
+        stop_value=1.5,
     ),
     policy=dict(
         cuda=True,
